91009af5dd1981e8c5cbfd2b5500f3ac6e7f3c71
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2025-05-15 22:30:17 +0800

    Update smart_pointer.r

--- base/smart_pointer/smart_pointer.r
+++ base/smart_pointer/smart_pointer.r
@@ -64,3 +64,42 @@ impl<T> Deref for MyBox<T>{
     编写函数和方法时,不需要添加太多显式的& 和 *
     允许编写能同时适用于引用和智能指针的代码
 */
+
+/*
+    Drop Trait 
+    用于定义一个值即将超出作用域时的清理行为
+
+    实现智能指针时几乎总是会用到Drop trait 的共嗯那个
+    Drop trait 只要求实现drop 方法，参数是对self的可变引用
+    Drop trait 在prelude 里,无手动引入
+*/
+
+Struct CustomSmartPointer {
+    data:String,
+
+}
+
+impol Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        // 输出销毁顺序，于创建顺序相反
+        println!("Dropping CustomSmartPointer with data `{}`",self.data);
+    }
+}
+
+fn main() {
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer{
+        data: String::from("other stuff"),
+    };
+    println!("CustomSmartPointer created");
+    //std::mem::drop(c) // c.drop() 显式调用drop方法不被允许
+    drop(c);
+    println!("CustomSmartPointer dropped before the end of main")
+}
+/*
+    如果想值在作用域结束前强制丢弃它，std::mem::drop 函数
+    它通过接管值的所有权(ownership) 并在调用后销毁它，避免商场释放(double free)问题
+
+*/
