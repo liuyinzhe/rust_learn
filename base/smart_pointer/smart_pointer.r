80d61844035b9ca1968ff2a6447a6d46b95cf263
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2025-05-20 22:27:15 +0800

    Update smart_pointer.r

--- base/smart_pointer/smart_pointer.r
+++ base/smart_pointer/smart_pointer.r
@@ -174,46 +174,47 @@ fn main() {
 */
 
 
-// pub trait  Messenger {
-//     fn send(&self,msg:&str);
+pub trait  Messenger {
+    fn send(&self,msg:&str);
 
-// }
+}
 
-// pub struct LimitTracker<'a,T:Messenger>{ // 实现了Messenger trait的泛型
-//     messenger: &'a T,
-//     value:usize,
-//     max:usize,
-// }
+pub struct LimitTracker<'a,T:Messenger>{ // 实现了Messenger trait的泛型
+    messenger: &'a T,
+    value:usize,
+    max:usize,
+}
 
-// impl<'a,T> LimitTracker<'a,T>
-// where 
-//     T:Messenger,
-// {
-//     pub fn new(messenger:&'a T,max:usize) -> LimitTracker<'a,T>{
-//         LimitTracker { 
-//             messenger,
-//             value,
-//             max,
-//         }
-//     }
+impl<'a,T> LimitTracker<'a,T>
+where 
+    T:Messenger,
+{
+    pub fn new(messenger:&'a T,max:usize) -> LimitTracker<'a,T>{
+        LimitTracker { 
+            messenger,
+            value,
+            max,
+        }
+    }
 
-//     pub fn set_value(&mut self, value:uszie){
-//         self.value= value;
+    pub fn set_value(&mut self, value:uszie){
+        self.value= value;
 
-//         let percentage_of_max = self.value as f64 /self.max as f64;
-//         if percentage_of_max >= 1.0 {
-//             self.messenger.send("Error: You are over your quota!");
-//         }else if percentage_of_max >= 0.9 {
-//             self.messenger
-//                 .send("Urgent warning: You've used up over 90% of you quota!");
-//         }else if percentage_of_max >= 0.75 {
-//             self.messenger
-//                 .send("Warning: You've used up over 75% of you quota!");
-//         }
+        let percentage_of_max = self.value as f64 /self.max as f64;
+        if percentage_of_max >= 1.0 {
+            self.messenger.send("Error: You are over your quota!");
+        }else if percentage_of_max >= 0.9 {
+            self.messenger
+                .send("Urgent warning: You've used up over 90% of you quota!");
+        }else if percentage_of_max >= 0.75 {
+            self.messenger
+                .send("Warning: You've used up over 75% of you quota!");
+        }
         
-//     }
-// }
+    }
+}
 
+#[cfg(test)]
 mod tests{
     use super::*;
     use std::cell::RefCell;
@@ -231,8 +232,43 @@ mod tests{
             self.sent_messages.borrow_mut().push(String::from(message));// borrow_mut 可变引用
         }
     }
+
+    #[test]
+    fn it_sends_an_over_75_percent_warning_message() {
+        let mock_messenger =  MockMessenger::new();
+        let mut limit_tracker = LimitTracker::new(&mock_messenger,90);
+        limit_tracker.set_value(80);
+        assert_eq!(mock_messenger.borrow().sent_messages.len(),1);
+    }
 }
 
-// borrow() -> Ref<T> 不可修改借用
-// borrow_mut() -> RefMut<T> 可修改借用
+// borrow() -> Ref<T> 获得不可修改借用
+// borrow_mut() -> RefMut<T> 获得可修改借用
 // 只能借用一次
+
+
+#[derive(Debug)]
+enum List {
+    Cons(Rc<RefCell<i32>>,Rc<List>),
+    Nil,
+}
+
+use crate::List::{Cons,Nil};
+use std::cell::RefCell;
+use std::rc::Rc;
+// Rc 与 RefCell 联合使用，修改不可变变量
+fn main(){
+    let value = Rc::new(RefCell::new(5));
+
+    // Rc 计数引用 clone
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+    // 链表 b c链向 a
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+    // 解引用 RefCell<T> 使用可变借用borrow_mut()
+    *value.borrow_mut() += 10;
+    println!("a after = {a:?}");
+    println!("b after = {b:?}");
+    println!("c after = {c:?}");
+}
