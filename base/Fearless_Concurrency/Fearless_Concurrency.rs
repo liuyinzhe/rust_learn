1bfb1227a4626d694a777097504dcd317ddc558f
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2025-05-26 23:14:34 +0800

    Update Fearless_Concurrency.rs

--- base/Fearless_Concurrency/Fearless_Concurrency.rs
+++ base/Fearless_Concurrency/Fearless_Concurrency.rs
@@ -62,7 +62,119 @@ fn main(){
     消息传递
     message passing
 
-    线程actors  通过发送包含数据的消息来互相通信
+    线程actors 通过发送包含数据的消息来互相通信
     
     Rust 的标准库提供了通道(channel)的实现
+
+    通道
+    channel
+    通道是一种程序设计概念，用于在不同线程之间发送数据
+    发送端(transmitter) 接收端(receiver)
+    
+    当通道的任意一端被丢弃时，通道被关闭
+
 */
+
+use std::sync::mpsc::{self, Receiver};
+use std::thread;
+use std::time::Duration;
+fn main(){
+    let (tx,rx) = mpsc::channel();
+    thread::spawn(move || { // move 转移所有权
+        // let val = String::from("hi");
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),  
+        ];
+        for val in vals{
+            tx.send(val).unwrap(); // 发送
+            thread::sleep(Duration::from_secs(1));
+        }
+
+        /*
+        send 接受一个要发送的值参数,丢弃时返回错误
+        返回一个Result<T,E>
+         */
+    });
+
+    
+    // let received = rx.recv().unwrap(); // recv 阻塞当前线程，直到接收到一个值
+    for received in rx {
+        // try_recv 方法 不会阻塞，二十立即返回一个Result
+        // 如果当前有消息可用，则返回Ok包含该内消息
+        // 如果当前没有信息，则返回Err
+        println!("Got: {received}");
+    }
+
+}
+
+/*
+    共享数据
+    让多个线程访问相同的共享数据
+
+    在某种程度上
+        任何编程语言中的通道都类似于单一所有权
+        共享内存并发就像多重所有权:多个线程可以同时访问相同的内存位置
+
+    Mutex 互斥锁
+
+    互斥锁在任何给定时间只允许一个线程访问某些数据
+    要访问互斥锁中的数据,线程必须请求获取互斥锁的锁(所有权)
+    锁时互斥锁的一种数据结构,用于跟踪谁当前拥有对数据的独占访问权
+    互斥锁被描述为通过锁定系统来保护它所持有的数据
+
+    Mutex 两条规则
+    使用数据之前，必须尝试获取锁
+    当使用完互斥锁保护的数据后，必须解锁数据,以便其他线程可以获取锁
+
+*/
+use std::sync::Mutex;
+// 单线程
+fn main(){
+    let m = Mutex::new(5);
+    // 作用域
+    {
+        let mut num = m.lock().unwrap(); // 返回LockResult<MutexGuard<T>> 
+        *num = 6; // 通过解引用修改被保护的值，解引用智能指针
+    }// MutexGuard实现了Drop trait，离开作用域自动释放锁
+
+    println!("m = {m:?}"); // m = Mutex { data:6, poisoned: false, ..}
+
+}
+/*
+使用Arc<T>进行原子引用计数
+
+Arc<T> 是一种类似Rc<T>的类型,可以安全地在并发环境中使用
+
+A 代表 atomic(原子性), 意味着它是一种原子引用计数类型
+参考标准库文档中的 std::sync::atomic
+
+缺点:线程安全会带来性能损失
+*/
+
+// 10个线程
+use std::sync::Mutex;
+use std::thread;
+use std::sync::Arc;
+
+fn main() {
+    let counter: Arc<Mutex<i32>> = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter) // 克隆counter
+        let handle = thread:spawn(move || { // move 会移动counter 所有权，导致第二个循环无法使用counter // 基于Arc 智能指针和克隆解决
+            let mut num = counter.lock().unwrap(); // 获得锁定的数据智能指针
+
+            *num + = 1;// 解引用 修改数值
+        });
+        handles.push(handle);
+    }
+    
+    for handle in handles {
+        handle.join().unwrap();// thread join 等待多线程执行完毕
+    }
+    println!("Result: {}",*counter.lock().unwrap());
+}
