fd438af5967e181bb0662a2cd415b5f164e76fd2
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2025-06-29 12:57:07 +0800

    Update main.rs

--- example/TcpListener_hello/src/main.rs
+++ example/TcpListener_hello/src/main.rs
@@ -5,8 +5,11 @@ use std::{
 use hello::ThreadPool;
 
 fn main() {
+    // 绑定监听
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    // 建立 线程池[对象:[workers列表,sender通信]]
     let pool =ThreadPool::new(4);
+    // 只处理2个连接(take(2)演示用)
     for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
 
@@ -25,6 +28,13 @@ fn handle_connection(mut stream: TcpStream) {
     //     .map(|result| result.unwrap())
     //     .take_while(|line| !line.is_empty())
     //     .collect();
+    
+
+    // buf_reader.lines() 返回一个迭代器，产生 io::Result<String> 类型即 (Result<String, std::io::Error>)
+    // .next() 获取迭代器的第一个元素，返回类型是 Option<io::Result<String>>
+    // Option<(io::Result<String, std::io::Error>>
+    // 第一个处理 Option  Some,None
+    // 第二个处理 Result  String,std::io::Error
     let request_line= buf_reader.lines().next().unwrap().unwrap();
 
     // if request_line.starts_with("GET / HTTP/1.1") { // http://localhost:7878
