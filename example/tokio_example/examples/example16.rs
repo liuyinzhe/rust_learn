817cc8cbe89f5144eacc8c9e47a676f81067247b
liuyinzhe <54565139+liuyinzhe@users.noreply.github.com>
2026-04-18 14:29:01 +0800

    Update example16.rs

--- example/tokio_example/examples/example16.rs
+++ example/tokio_example/examples/example16.rs
@@ -46,10 +46,13 @@
 //     println!("Counter = {}", *COUNTER.lock().unwrap());
 // }
 
+// 延迟初始化容器。它包裹的值在首次访问时才会执行初始化闭包，且只执行一次，之后返回同一实例的引用
+// 适用于全局 static 变量的惰性构造
 use std::{time::Duration, sync::LazyLock};
 
 use tokio::{sync::Mutex,time::sleep};
 
+// 闭包会在第一次访问 DATA 时被调用，生成一个 Mutex<u32> 实例。此后每次访问 DATA 都会得到同一个已初始化的互斥锁
 static DATA: LazyLock<Mutex<u32>> = LazyLock::new(|| Mutex::new(0));
 
 async fn task1() {
@@ -63,7 +66,7 @@ async fn task1() {
 async fn task2() {
     sleep(Duration::from_millis(100)).await;
     println!("Task2 try lock");
-    let _guard = DATA.lock().await;
+    let _guard = DATA.lock().await; // 获取锁 ，阻塞等待
     println!("Task2 locked");
     println!("Task2 done");
 }
